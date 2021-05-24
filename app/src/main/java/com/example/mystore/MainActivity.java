ca0a31aa98b62a23591bcc022f3d3e6e94222a2f
Pratistha <rinsin2002@gmail.com>
2021-05-24 18:00:13 +0530

    Login layout

--- app/src/main/java/com/example/mystore/MainActivity.java
+++ app/src/main/java/com/example/mystore/MainActivity.java
@@ -16,9 +16,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
 
-                Intent mainIntent = new Intent(MainActivity.this, MainActivity2.class);
+                Intent mainIntent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(mainIntent);
             }
         }, 3000);
     }
 }
+/*
+<androidx.viewpager.widget.ViewPager
+            android:id="@+id/viewPager"
+            android:layout_width="399dp"
+            android:layout_height="411dp"
+            app:layout_constraintBottom_toBottomOf="parent"
+            app:layout_constraintEnd_toEndOf="parent"
+            app:layout_constraintHorizontal_bias="0.0"
+            app:layout_constraintStart_toStartOf="parent"
+            app:layout_constraintTop_toTopOf="parent"
+            app:layout_constraintVertical_bias="0.206">
+ */
