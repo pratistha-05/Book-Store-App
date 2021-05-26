11bd958367a882297f3af0877c86ad3c4880b9b6
Pratistha <rinsin2002@gmail.com>
2021-05-26 20:41:18 +0530

    onboarding screens

--- app/src/main/java/com/example/mystore/MainActivity.java
+++ app/src/main/java/com/example/mystore/MainActivity.java
@@ -38,23 +38,6 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        /*background=findViewById(R.id.imageView);
-        appName = findViewById(R.id.textView);
-        animation=findViewById(R.id.animationView);
-
-        viewPager = findViewById(R.id.pager);
-
-        pagerAdapter=new ScreenSlideAdapter(getSupportFragmentManager());
-        viewPager.setAdapter(pagerAdapter);
-
-
-
-        background.animate().translationY(-3000).setDuration(1000).setStartDelay(4000);
-        appName.animate().translationY(2000).setDuration(1000).setStartDelay(4000);
-        animation.animate().translationY(2000).setDuration(1000).setStartDelay(4000);
-       viewPager.setVisibility(View.INVISIBLE);
-
-          */
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -68,54 +51,5 @@ public class MainActivity extends AppCompatActivity {
     }
 }
 
-
-
-   /* private class ScreenSlideAdapter extends FragmentStatePagerAdapter{
-
-        public ScreenSlideAdapter(@NonNull FragmentManager fm) {
-            super(fm);
-        }
-
-        @NonNull
-        @Override
-        public Fragment getItem(int position) {
-
-            switch(position) {
-
-                case 0:
-                    Onboarding_fragment1 tab1=new Onboarding_fragment1();
-                    return tab1;
-                case 1:
-                    Onboading_fragment2 tab2=new Onboading_fragment2();
-                    return tab2;
-                case 2:
-                    Onboarding_fragment3 tab3=new Onboarding_fragment3();
-                    return tab3;
-            }
-            return null;
-        }
-
-        @Override
-        public int getCount() {
-            return NUM_PAGES;
-        }
-    }
-
-
-
-
-    }
-
-
-/*   <androidx.viewpager.widget.ViewPager
-        android:id="@+id/pager"
-        android:layout_width="match_parent"
-        android:layout_height="wrap_content"
-        app:layout_constraintBottom_toBottomOf="parent"
-        app:layout_constraintLeft_toLeftOf="parent"
-        app:layout_constraintRight_toRightOf="parent"
-        app:layout_constraintTop_toTopOf="parent"/>
-
- */
 
 
